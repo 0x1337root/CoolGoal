74775cfce322f7fa167a048610d9421d3256b5df
omfacu <57021647+omfacu@users.noreply.github.com>
2021-01-13 22:44:52 +0300

    CoolGoal

---
+++ CoolGoal/Assets/Scripts/BezierFollow.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BezierFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] routes;
+
+    private int routeToGo;
+
+    private float tParam;
+
+    private Vector3 ballPosition;
+
+    private Vector3 startPosition;
+
+    private float speedModifier;
+
+    private bool coroutineAllowed;
+
+    void Start()
+    {
+        routeToGo = 0;
+        tParam = 0f;
+        speedModifier = 1.25f;
+        coroutineAllowed = true;
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && Input.mousePosition.y < 600)
+        {
+            if (coroutineAllowed)
+            {
+                StartCoroutine(GoByTheRoute(routeToGo));
+            }
+        }
+    }
+
+    private IEnumerator GoByTheRoute(int routeNumber)
+    {
+        coroutineAllowed = false;
+
+        Vector3 p0 = routes[routeNumber].GetChild(0).position;
+        Vector3 p1 = routes[routeNumber].GetChild(1).position;
+        Vector3 p2 = routes[routeNumber].GetChild(2).position;
+
+        while (tParam < 1)
+        {
+            tParam += Time.deltaTime * speedModifier;
+
+            ballPosition = Mathf.Pow(1 - tParam, 2) * p0 +
+                        2 * Mathf.Pow(1 - tParam, 1) * tParam * p1 +
+                        Mathf.Pow(tParam, 2) * p2;
+
+            transform.position = ballPosition;
+            yield return new WaitForEndOfFrame();
+        }
+
+        tParam = 0f;
+
+        routeToGo += 1;
+
+        transform.position = startPosition;
+
+        if(routeToGo > routes.Length - 1)
+        {
+            routeToGo = 0;
+        }
+
+        coroutineAllowed = true;
+    }
+}
---
+++ CoolGoal/Assets/Scripts/Rota.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class Rota : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] controlPoints;
+
+    private Vector3 gizmosPosition;
+
+    private Vector3 mPosition;
+
+    private Vector3 p1Pos;
+
+    private Vector3 p2Pos;
+
+    private float x1;
+
+    private float x2;
+
+    private float x3;
+
+    private void Start()
+    {
+        p1Pos = controlPoints[1].position;
+        p2Pos = controlPoints[2].position;
+    }
+
+    private void FixedUpdate()
+    {
+        mPosition = Input.mousePosition;
+
+        x2 = (mPosition.x - x1) / 60f;
+        x3 = (mPosition.x - x1) / 115.18f;
+
+        p1Pos.x = x2;
+        p2Pos.x = x3;
+
+        if (p1Pos.x >= 4.5f)
+        {
+            p1Pos.x = 4.5f;
+        }
+
+        else if(p1Pos.x <= -4.5f)
+        {
+            p1Pos.x = -4.5f;
+        }
+
+        if(p2Pos.x >= 3.5f)
+        {
+            p2Pos.x = 3.5f;
+        }
+
+        else if(p2Pos.x <= -3.5f)
+        {
+            p2Pos.x = -3.5f;
+        }
+
+        controlPoints[1].position = p1Pos;
+        controlPoints[2].position = p2Pos;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            x1 = mPosition.x;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (mPosition.y < 600)
+            {
+                for (float t = 0.05f; t <= 1; t += 0.05f)
+                {
+                    gizmosPosition = Mathf.Pow(1 - t, 2) * controlPoints[0].position +
+                        2 * Mathf.Pow(1 - t, 1) * t * controlPoints[1].position +
+                        Mathf.Pow(t, 2) * controlPoints[2].position;
+
+                    Gizmos.color = Color.magenta;
+                    Gizmos.DrawSphere(gizmosPosition, 0.1f);
+                }
+            }
+        }
+    }
+}
